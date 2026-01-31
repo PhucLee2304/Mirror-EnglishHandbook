86e37ec2cfadd88d077420fb36c6c4cff4d20ceb
Phuc <miracle23042004@gmail.com>
2026-01-31 09:52:35 +0700

    feat(auth): add logs

--- core/pkg/gateways/firebase/firebase.go
+++ core/pkg/gateways/firebase/firebase.go
@@ -21,19 +21,29 @@ func NewClient(cfg *config.Config) (*Client, error) {
 	ctx := context.Background()
 
 	opt := option.WithCredentialsFile(cfg.FirebaseCredentialsPath)
+	log.Println(cfg.FirebaseCredentialsPath)
 	app, err := firebase.NewApp(ctx, nil, opt)
+	if app == nil {
+		log.Println("app init failed")
+	}
 	if err != nil {
 		log.Printf("error creating firebase app: %v\n", err)
 		return nil, err
 	}
 
 	authClient, err := app.Auth(ctx)
+	if authClient == nil {
+		log.Println("auth init failed")
+	}
 	if err != nil {
 		log.Printf("error getting auth client: %v\n", err)
 		return nil, err
 	}
 
 	messagingClient, err := app.Messaging(ctx)
+	if messagingClient == nil {
+		log.Println("messaging init failed")
+	}
 	if err != nil {
 		log.Printf("error getting messaging client: %v\n", err)
 		return nil, err
@@ -43,10 +53,22 @@ func NewClient(cfg *config.Config) (*Client, error) {
 }
 
 func (c *Client) VerifyIDToken(ctx context.Context, idToken string) (*auth.Token, error) {
+	log.Println("idtoken: " + idToken)
 	token, err := c.auth.VerifyIDToken(ctx, idToken)
 	if err != nil {
+		if auth.IsIDTokenExpired(err) {
+			log.Printf("id token expired: %v\n", err)
+		} else if auth.IsIDTokenInvalid(err) {
+			log.Printf("id token invalid: %v\n", err)
+		} else if auth.IsIDTokenRevoked(err) {
+			log.Printf("id token revoked: %v\n", err)
+		} else {
+			log.Printf("error verifying id token: %v\n", err)
+		}
 		return nil, err
 	}
+	log.Printf("token.Audience: %v\n", token.Audience)
+	log.Printf("token.Issuer: %v\n", token.Issuer)
 
 	return token, nil
 }
